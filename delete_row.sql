efb9cb20501eba7a2cf82de52cef60501a51b8e8
rid47 <ridwanmizan@gmail.com>
2023-01-09 06:45:58 +0600

    create corporate db

---
+++ delete_row.sql
@@ -0,0 +1,5 @@
+use ridwan;
+select * from student;
+
+delete from student 
+where student_id=1
---
+++ upate_column.sql
@@ -0,0 +1,8 @@
+use ridwan;
+select * from student;
+
+update student 
+
+set major = 'Bio'
+
+where major = 'Biology'
